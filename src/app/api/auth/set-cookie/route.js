49726657e21e379142eb94e0bf5c5b7d335460c8
mostafa <mostafatam79@gmail.com>
2025-12-16 10:26:13 +0200

    remove the body from the request

--- src/app/api/auth/set-cookie/route.js
+++ src/app/api/auth/set-cookie/route.js
@@ -3,7 +3,6 @@ import { NextResponse } from 'next/server';
 
 export async function POST(request) {
   const body = await request.json();
-  console.log(body);
 
   const userData = JSON.stringify(body);
 
