fc1eff4b0b1efadec67c8acb5eecaa3292367cf3
mostafa <mostafatam79@gmail.com>
2026-01-15 16:39:50 +0200

    update the env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 const nextConfig = {
   // التكوينات الخاصة بـ Next.js
   env: {
-    NEST_PUBLIC_BASE_URL_2: 'http://localhost:3002',
-    TOKENJWT_SECRET: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImVtYWlsIjoiYWRtaW5Aam9lMTMuY29tIiwicm9sZSI6InN1cGVyYWRtaW4iLCJpYXQiOjE3NjU3MDYwMjgsImV4cCI6MTc2NTcwNjAyOH0.x2O3WS4sPgSAkuX8dYYlWf-3JHALiMBjEQ0Ed1UCeks',
+    NEST_PUBLIC_BASE_URL_2: `${process.env.NEST_PUBLIC_BASE_URL_2}`,
+    TOKENJWT_SECRET: `${process.env.TOKENJWT_SECRET}`,
   },
 };
 
